7fc9f09face1ab57ddf16107b8f9889121651ff0
Saurabh <raisaurabh265@gmail.com>
2022-08-01 17:38:06 +0530

    process.env.PORT added

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
 
 app.use("/health", healthRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port: http://localhost:3000");
 });
