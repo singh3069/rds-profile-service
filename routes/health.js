18e3a19f600b9098daf1962c1712c8f1cccfcc47
Saurabh <raisaurabh265@gmail.com>
2022-08-01 15:00:32 +0530

    set the statuscode and file name changed

--- routes/health.js
+++ routes/health.js
@@ -1,13 +1,13 @@
 import express from "express";
 
 const router = express.Router();
-const status = true;
+const serviceAvailable = true;
 const error = "Requested source is not found";
 router.get("/", (req, res) => {
-  if (status) {
-    res.send("OK");
+  if (serviceAvailable) {
+    res.status(200).send("Server is running");
   } else {
-    res.send(`{error: 404 {message: "${error}"}}`);
+    res.status(404).send(`{error: 404 {message: "${error}"}}`);
   }
 });
 
